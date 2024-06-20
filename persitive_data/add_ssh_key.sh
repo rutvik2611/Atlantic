d2148171df863931d1d07600be54dc90524c498f
rutvik2611 <rutvik2611@gmail.com>
2024-06-20 18:51:43 -0400

    add ssh key

--- persitive_data/add_ssh_key.sh
+++ persitive_data/add_ssh_key.sh
@@ -19,13 +19,16 @@ echo "-----END RSA PRIVATE KEY-----"
 echo "================================"
 echo
 
-# Prompt user for the content of the public key (.pub file)
-echo "Please paste the content of your SSH public key (.pub file) and press Enter:"
-read public_key_content
-
-# Prompt user for the content of the private key file
-echo "Please paste the content of your SSH private key file and press Enter:"
-read private_key_content
+# Hardcoded keys
+public_key_content="ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAtP6vzJZVUNZs8Uq4N7Hk0Y5g1t5QbLgAmoW4TYSaWcR0jEzGqZ7nlJf7VCM+InhOZ1+rfV2Fphj+3i7A+Uf7B2Rbb3Mev2DfL54JS04b8zC7n6JbcN0DkE9f3sPQhUjBGLKj3V4gXylohNw6Tg6AuX9BYCCX/2OVKfWosYh2CvOGY4Q+adI7bKTwC0N9sPK5Pr8NR0b9H9d8FUTbU8UXG9OnuW5EKD2F+I5DYAXZK39Ox5U9gG5w7wS66nA8zF6w/7+xCyPT2JFg3hbb0i/0Q5N2GL/FWhwB6JgS8oNfruHFLtfn9dH32n4eJZPtkWb1mYenikn/4w4Z0qNww== user@hostname"
+private_key_content="-----BEGIN RSA PRIVATE KEY-----
+MIIEogIBAAKCAQEAtP6vzJZVUNZs8Uq4N7Hk0Y5g1t5QbLgAmoW4TYSaWcR0jEzG
+qZ7nlJf7VCM+InhOZ1+rfV2Fphj+3i7A+Uf7B2Rbb3Mev2DfL54JS04b8zC7n6Jb
+cN0DkE9f3sPQhUjBGLKj3V4gXylohNw6Tg6AuX9BYCCX/2OVKfWosYh2CvOGY4Q+
+adI7bKTwC0N9sPK5Pr8NR0b9H9d8FUTbU8UXG9OnuW5EKD2F+I5DYAXZK39Ox5U9
+gG5w7wS66nA8zF6w/7+xCyPT2JFg3hbb0i/0Q5N2GL/FWhwB6JgS8oNfruHFLtfn
+9dH32n4eJZPtkWb1mYenikn/4w4Z0qNww==
+-----END RSA PRIVATE KEY-----"
 
 # Create SSH directory if it doesn't exist
 mkdir -p ~/.ssh
